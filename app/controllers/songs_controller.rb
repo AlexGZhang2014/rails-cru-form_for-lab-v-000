63a6f5c8472ff4d672dbd96e7e9f6e9b78803a96
Alex Zhang <alexgzhang92@gmail.com>
2018-07-06 02:40:48 +0000

    automatically backed up by learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,34 @@
+class SongsController < ApplicationController
+  def index
+    @song = Song.all
+  end
+  
+  def new
+    @song = Song.new
+  end
+  
+  def create
+    @song = Song.create(artist_params(:name))
+    redirect_to song_path(@song)
+  end
+  
+  def show
+    @song = Song.find(params[:id])
+  end
+  
+  def edit
+    @song = Song.find(params[:id])
+  end
+  
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name))
+    redirect_to song_path(@song)
+  end
+  
+  private
+  
+    def song_params(*args)
+      params.require(:song).permit(*args)
+    end
+end
